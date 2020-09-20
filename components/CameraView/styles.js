120f76365c9c4a68aacbc0e29bd7116e57a7b1a4
AnasMeshal <Fire-Ball-KwT@hotmail.com>
2020-09-20 17:50:07 +0300

    moved this into a folder and created the styles for it

---
+++ components/CameraView/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { View } from "native-base";
+
+export const LiveScanScreen = styled(View)`
+  border-radius: 10px;
+  align-self: center;
+  border: 5px blue;
+  width: 200px;
+  height: 200px;
+`;
+
+export const ResultStyled = styled.Text`
+  font-weight: bold;
+  margin: 5px;
+  align-self: center;
+  font-size: 25px;
+  margin-bottom: 25px;
+  color: white;
+  margin-top: 15px;
+  text-align: center;
+`;
