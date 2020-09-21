583b47cf61902bd028249f9c5ff5ce562f9daf8e
Sarah <S00038939@auk.edu.kw>
2020-09-21 20:47:48 +0300

    Added and used modal component in identification component

--- components/Identification/index.js
+++ components/Identification/index.js
@@ -11,18 +11,18 @@ import Camera from "../Buttons/Camera";
 
 // Components
 import CameraView from "../CameraView";
+import Modal from "../Modal";
 
 // Styles
-import { Spinner, Text } from "native-base";
+import { Spinner } from "native-base";
+import { TouchableOpacity } from "react-native";
 import {
   DetectTextStyled,
   ImagePreviewStyled,
-  ResultStyled,
   BackgroundImage,
   DarkView,
   ButtonsRow,
 } from "./styles";
-import { ScrollView } from "react-native";
 
 const Identification = () => {
   const [imageUrl, setImageUrl] = useState();
@@ -30,31 +30,13 @@ const Identification = () => {
   const [liveResult, setLiveResult] = useState("");
   const [loading, setLoading] = useState(false);
   const [live, setLive] = useState(false);
-  const [nutrition, setNutrition] = useState("hello");
+  const [nutrition, setNutrition] = useState("");
+  const [openModal, setOpenModal] = useState(false);
 
   const identifyImage = async (imageData, isLive) => {
     const app = new Clarifai.App({
       apiKey: "0352be76758845c794f90c92cdbcac5d",
     });
-
-    let test = "";
-    const myWolframAppId = "425X9Q-JEJJ2Q5LJ6";
-
-    fetch(
-      "http://api.wolframalpha.com/v2/query?input=banana%20nutrition%20facts&appid=425X9Q-JEJJ2Q5LJ6"
-    )
-      .then((response) => response.text())
-      .then((response) => {
-        parseString(response, function (err, result) {
-          test = response;
-          parseString(test, function (err, result) {
-            setNutrition(result.queryresult.pod[1].subpod[0].img[0].$.alt);
-          });
-        });
-      })
-      .catch((err) => {
-        console.log("fetch", err);
-      });
 
     try {
       const res = await app.models.predict(Clarifai.FOOD_MODEL, imageData);
@@ -65,19 +47,34 @@ const Identification = () => {
         );
       } else if (isLive) {
         setLiveResult("Detected " + detectedObject);
+        fetchNutrition(detectedObject);
       } else {
-
         setResult("Detected " + detectedObject);
-        test =
-          "http://api.wolframalpha.com/v2/query?input=" +
-          detectedObject +
-          "%20nutrition%20facts&appid=" +
-          myWolframAppId;
 
+        fetchNutrition(detectedObject);
       }
       setLoading(false);
+      setOpenModal(true);
+      setLoading(true);
     } catch (error) {
       setResult("This item cannot be identified. Please try again.");
+    }
+  };
+
+  const fetchNutrition = async (detectedObject) => {
+    try {
+      setNutrition("");
+      const detectedObjectUrl =
+        "http://api.wolframalpha.com/v2/query?input=" +
+        detectedObject +
+        "%20nutrition%20facts&appid=425X9Q-JEJJ2Q5LJ6";
+      const response = await fetch(detectedObjectUrl);
+      parseString(await response.text(), function (err, result) {
+        setNutrition(result.queryresult.pod[1].subpod[0].img[0].$.alt);
+      });
+      setLoading(false);
+    } catch (err) {
+      console.log("fetch", err);
     }
   };
 
@@ -86,13 +83,26 @@ const Identification = () => {
     <BackgroundImage source={require("../../assets/background.jpg")}>
       <DarkView>
         <DetectTextStyled>Detect</DetectTextStyled>
-
-        {/* {imageUrl && live === false && (
-          <ImagePreviewStyled
-            source={{ uri: imageUrl }}
-            style={{ width: 200, height: 200 }}
+        {openModal && (
+          <Modal
+            live={live}
+            imageUrl={imageUrl}
+            nutrition={nutrition}
+            result={result}
+            openModal={openModal}
+            setOpenModal={setOpenModal}
+            loading={loading}
           />
-        )} */}
+        )}
+
+        {imageUrl && live === false && openModal === false && (
+          <TouchableOpacity onPress={() => setOpenModal(true)}>
+            <ImagePreviewStyled
+              source={{ uri: imageUrl }}
+              style={{ width: 200, height: 200 }}
+            />
+          </TouchableOpacity>
+        )}
 
         {live && (
           <CameraView
@@ -103,29 +113,28 @@ const Identification = () => {
             identifyImage={identifyImage}
           />
         )}
-        {loading && live === false ? (
+
+        {loading && live === false && openModal === false && (
           <Spinner color="white" />
-        ) : (
-          live === false && <ResultStyled>{result}</ResultStyled>
         )}
-        <ScrollView>
-          <ResultStyled>{nutrition}</ResultStyled>
-        </ScrollView>
-        {live === false && (
-          <ButtonsRow>
-            <CameraRoll
-              setImageUrl={setImageUrl}
-              setLoading={setLoading}
-              identifyImage={identifyImage}
-            />
-            <Camera
-              setImageUrl={setImageUrl}
-              setLoading={setLoading}
-              identifyImage={identifyImage}
-            />
-          </ButtonsRow>
+
+        {live === false && openModal === false && (
+          <>
+            <ButtonsRow>
+              <CameraRoll
+                setImageUrl={setImageUrl}
+                setLoading={setLoading}
+                identifyImage={identifyImage}
+              />
+              <Camera
+                setImageUrl={setImageUrl}
+                setLoading={setLoading}
+                identifyImage={identifyImage}
+              />
+            </ButtonsRow>
+            <LiveScan setLive={setLive} screen />
+          </>
         )}
-        {live === false && <LiveScan setLive={setLive} screen />}
       </DarkView>
     </BackgroundImage>
   );
