b5f987958d96b6b0f6f9d833b026f41a65ac4e34
Sarah <S00038939@auk.edu.kw>
2020-09-27 18:53:30 +0300

    updated CameraView function

--- components/CameraView/index.js
+++ components/CameraView/index.js
@@ -1,14 +1,123 @@
+// import React, { useState, useEffect, useRef } from "react";
+
+// // Libraries
+// import { Camera } from "expo-camera";
+// import * as FileSystem from "expo-file-system";
+
+// // Buttons
+// import LiveScan from "../Buttons/LiveScan";
+
+// // Styles
+// import { Text, View } from "react-native";
+// import { LiveScanScreen, SpinnerLoading } from "./styles";
+// import { TouchableOpacity } from "react-native-gesture-handler";
+
+// const CameraView = ({
+//   identifyImage,
+//   setLoading,
+//   loading,
+//   result,
+//   setImageUrl,
+//   setLive,
+//   setOpenModal,
+//   setResult,
+//   setNutrition,
+//   navigation,
+// }) => {
+//   const [hasPermission, setHasPermission] = useState(null);
+
+//   const cam = useRef();
+
+// const handleLiveScan = () => {
+//   if (cam.current) {
+//     setTimeout(async () => {
+//       const picture = await cam.current.takePictureAsync({ quality: 1 });
+//       setImageUrl(picture.uri);
+//       const base64 = await FileSystem.readAsStringAsync(picture.uri, {
+//         encoding: "base64",
+//       });
+//       identifyImage(base64, {
+//         setResult,
+//         setLoading,
+//         setLive,
+//         setOpenModal,
+//         setNutrition,
+//         navigation,
+//       });
+//       setLoading(true);
+//     }, 2000);
+//   }
+// };
+//   //TODO: CLEAN THIS MESS
+//   useEffect(() => {
+//     (async () => {
+//       const { status } = await Camera.requestPermissionsAsync();
+//       setHasPermission(status === "granted");
+//     })();
+//   }, []);
+
+//   if (hasPermission === null) {
+//     return <View />;
+//   }
+//   if (hasPermission === false) {
+//     return <Text>No access to camera</Text>;
+//   }
+//   return (
+//     <>
+//       {/* <LiveScanScreen> */}
+//       <View>
+//         <Camera style={{ flex: 1 }}>
+//           <View
+//             style={{
+//               flex: 1,
+//               backgroundColor: "transparent",
+//               flexDirection: "row",
+//             }}
+//           >
+//             <TouchableOpacity
+//               style={{
+//                 flex: 0.1,
+//                 alignSelf: "flex-end",
+//                 alignItems: "center",
+//               }}
+//               onPress={() => {
+//                 setType(
+//                   type === Camera.Constants.Type.back
+//                     ? Camera.Constants.Type.front
+//                     : Camera.Constants.Type.back
+//                 );
+//               }}
+//             >
+//               <Text
+//                 style={{ fontSize: 18, marginBottom: 10, color: "white" }}
+//               ></Text>
+//             </TouchableOpacity>
+//           </View>
+//         </Camera>
+//       </View>
+//       {/* <Camera style={{ flex: 1 }} ref={cam} onCameraReady={handleLiveScan}>
+//         <Text>this is a test</Text>
+//       </Camera> */}
+//       {/* </LiveScanScreen> */}
+//       {loading && <SpinnerLoading color="white" />}
+//       {result !== "" && loading === false && <LiveScan setLive={setLive} />}
+//     </>
+//   );
+// };
+
+// export default CameraView;
+
 import React, { useState, useEffect, useRef } from "react";
-
-// Libraries
+import { Text, View, TouchableOpacity } from "react-native";
 import { Camera } from "expo-camera";
-import * as FileSystem from "expo-file-system";
-
-// Buttons
 import LiveScan from "../Buttons/LiveScan";
 
+// Libraries
+
+import * as FileSystem from "expo-file-system";
+
 // Styles
-import { Text, View } from "react-native";
+
 import { LiveScanScreen, SpinnerLoading } from "./styles";
 
 const CameraView = ({
@@ -23,10 +132,7 @@ const CameraView = ({
   setNutrition,
   navigation,
 }) => {
-  const [hasPermission, setHasPermission] = useState(null);
-
   const cam = useRef();
-
   const handleLiveScan = () => {
     if (cam.current) {
       setTimeout(async () => {
@@ -36,8 +142,8 @@ const CameraView = ({
           encoding: "base64",
         });
         identifyImage(base64, {
-          setResult,
           setLoading,
+          setResult,
           setLive,
           setOpenModal,
           setNutrition,
@@ -47,7 +153,10 @@ const CameraView = ({
       }, 2000);
     }
   };
-  //TODO: CLEAN THIS MESS
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
@@ -62,14 +171,50 @@ const CameraView = ({
     return <Text>No access to camera</Text>;
   }
   return (
-    <>
-      <LiveScanScreen>
-        <Camera style={{ flex: 1 }} ref={cam} onCameraReady={handleLiveScan} />
-      </LiveScanScreen>
-      {loading && <SpinnerLoading color="white" />}
-      {result !== "" && loading === false && <LiveScan setLive={setLive} />}
-    </>
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} type={type} ref={cam}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "transparent",
+            flexDirection: "row",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              flex: 0.1,
+              alignSelf: "flex-end",
+              alignItems: "center",
+            }}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={{ fontSize: 40, marginBottom: 10, color: "white" }}>
+              Flips
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              flex: 0.1,
+              alignSelf: "flex-end",
+              alignItems: "center",
+            }}
+            onPress={() => handleLiveScan()}
+          >
+            <Text style={{ fontSize: 18, marginBottom: 20, color: "white" }}>
+              test
+            </Text>
+          </TouchableOpacity>
+          {loading && <SpinnerLoading color="white" />}
+          {result !== "" && loading === false && <Text>{result}</Text>}
+        </View>
+      </Camera>
+    </View>
   );
 };
-
 export default CameraView;
